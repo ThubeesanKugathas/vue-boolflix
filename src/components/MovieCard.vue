2b196ad120e183f759902e5d21384c30daa43b72
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-03 15:47:16 +0100

    card styling

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="ms_card-box position-relative">
+        <h2>MOVIE</h2>
         <img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`" :alt="movie.title">
         <div class="ms_hover position-absolute top-0 start-0 px-3 py-5">
             <p><strong>Title: </strong>{{movie.title}}</p>
@@ -30,36 +31,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .ms_card-box {
-        height: 500px;
-        width: calc((100% - 80px) / 3);
-        margin: 10px;
-        overflow: hidden;
-
-        &:hover .ms_hover {
-            display: block;
-        }
-
-        img {
-            width: 100%;
-        }
-
-        .ms_hover {
-            background-color: #000;
-            height: 100%;
-            width: 100%;
-            color: #fff;
-            overflow-y: auto;
-            display: none;
-
-            p {
-                margin: 0;
-                font-size: 20px;
-            }
-
-            strong {
-                font-size: 25px;
-            }
-        }
-    }
+@import '../style/card.scss';
 </style>

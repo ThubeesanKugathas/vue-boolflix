7692fcb75ed50805ba5989a3a1d6e16220387370
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-04 15:30:37 +0100

    empty stars implemented in MovieCard and in TvCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -5,12 +5,20 @@
         <div class="ms_hover position-absolute top-0 start-0 px-3 py-5">
             <p><strong>Title: </strong>{{movie.title}}</p>
             <p><strong>Original Title: </strong>{{movie.original_title}}</p>
-            <p>
-                <strong>Rating: </strong>
+            <div class="d-flex align-items-center">
+                <p>
+                    <strong>Rating: </strong>
+                </p>
                 <i class="fas fa-star" 
                     v-for="(star, i) in Math.round(movie.vote_average / 2)" 
-                    :key="i"></i>
-            </p>
+                    :key="i">
+                </i>
+                <div v-if="(5 - Math.round(movie.vote_average / 2)) > 0">
+                    <i class="far fa-star"
+                        v-for="(nostar, index) in (5 - Math.round(movie.vote_average / 2))"
+                        :key="index"></i>
+                </div>
+            </div>
             <p>
                 <strong>Originl Language: </strong>{{movie.original_language.toUpperCase()}} 
                 <img :src="`/flags/${movie.original_language}.png`" :alt="movie.original_language">
--- src/components/TvCard.vue
+++ src/components/TvCard.vue
@@ -5,12 +5,20 @@
         <div class="ms_hover position-absolute top-0 start-0 px-3 py-5">
             <p><strong>Name: </strong>{{show.name}}</p>
             <p><strong>Original Name: </strong>{{show.original_name}}</p>
-            <p>
-                <strong>Rating: </strong>
+            <div class="d-flex align-items-center">
+                <p>
+                    <strong>Rating: </strong>
+                </p>
                 <i class="fas fa-star" 
                     v-for="(star, i) in Math.round(show.vote_average / 2)" 
-                    :key="i"></i>
-            </p>
+                    :key="i">
+                </i>
+                <div v-if="(5 - Math.round(show.vote_average / 2)) > 0">
+                    <i class="far fa-star"
+                        v-for="(nostar, index) in (5 - Math.round(show.vote_average / 2))"
+                        :key="index"></i>
+                </div>
+            </div>
             <p>
                 <strong>Originl Language: </strong>{{show.original_language.toUpperCase()}} 
                 <img :src="`/flags/${show.original_language}.png`" :alt="show.original_language">
